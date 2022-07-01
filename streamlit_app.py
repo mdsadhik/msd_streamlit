af447db829a97b7d65093ca8ac12336e1fcafe6a
mdsadhik <89764131+mdsadhik@users.noreply.github.com>
2022-07-01 10:03:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,6 +80,20 @@ def get_chart(data):
     return (lines + points + tooltips).interactive()
 
 space(2)
+
+st.write("Give more context to your time series using annotations!")
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    ticker = st.text_input("Choose a ticker (⬇💬👇ℹ️ ...)", value="⬇")
+with col2:
+    ticker_dx = st.slider(
+        "Horizontal offset", min_value=-30, max_value=30, step=1, value=0
+    )
+with col3:
+    ticker_dy = st.slider(
+        "Vertical offset", min_value=-30, max_value=30, step=1, value=-10
+    )
 # Original time series chart. Omitted `get_chart` for clarity
 source = get_data()
 chart = get_chart(source)
