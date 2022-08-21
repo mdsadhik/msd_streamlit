aa8383fe7016e9291a17f337be4a83e78d067aad
mdsadhik <89764131+mdsadhik@users.noreply.github.com>
2022-08-21 18:21:13 +0530

    Update read_write_csv.py

--- pages/read_write_csv.py
+++ pages/read_write_csv.py
@@ -1,8 +1,15 @@
-import pandas
+import pandas as pd
+import streamlit as st
 
-df = pandas.read_csv(
-	'ROLLOVER_DATA.csv'
-)
+from pathlib import Path
 
-df.to_csv('ROLLOVER_DATA_UPD.csv')
+csv_file_path = Path(__file__).parents[1] / 'data/ROLLOVER_DATA.csv'
+
+st.write(csv_file_path)
+print(csv_file_path)
+rollover_df = pd.read_csv(csv_file_path)  
+
+csv_file_path_updated= Path(__file__).parents[1] / 'data/ROLLOVER_DATA.csv'
+
+rollover_df.to_csv(csv_file_path_updated)
 
